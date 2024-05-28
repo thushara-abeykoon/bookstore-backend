598f4779fc11c24c6dc1598490bd32266a2aa65a
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-05-28 15:44:50 +0530

    changed a validator to be a super class to book validator and author validator

---
+++ src/main/java/com/silva/bookstore/service/util/BookCredentialsValidator.java
@@ -0,0 +1,21 @@
+package com.silva.bookstore.service.util;
+
+import com.silva.bookstore.model.Book;
+
+import java.util.regex.Pattern;
+
+public class BookCredentialsValidator extends Validator {
+
+    private final Pattern alphaNumeric = Pattern.compile("^[a-zA-Z0-9]+$");
+
+    private final Book book;
+    public BookCredentialsValidator(Book book) {
+        this.book = book;
+    }
+
+    public boolean isBookValid() {
+        return validate(alphaNumeric, book.getIsbn()) && validate(alphaNumeric, book.getTitle());
+    }
+
+
+}
