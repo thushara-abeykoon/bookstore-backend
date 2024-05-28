64e028bcb01da6e2871a8ad6d3eed3e08201babc
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-05-28 15:42:49 +0530

    added book service interface

---
+++ src/main/java/com/silva/bookstore/service/BookService.java
@@ -0,0 +1,20 @@
+package com.silva.bookstore.service;
+
+import com.silva.bookstore.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    public List<Book> getBooks();
+
+    public List<Book> searchBooks(String isbn);
+
+    public Book getBook(String isbn);
+
+    public void addBook(Book book);
+
+    public void updateBook(Book book, String isbn);
+
+    public void deleteBook(String isbn);
+
+}
