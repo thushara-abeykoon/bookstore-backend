9c84f65928e3f0151adc074c0399ec0873040cae
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-05-28 14:29:47 +0530

    added getters to all the attributes including id

--- src/main/java/com/silva/bookstore/model/Author.java
+++ src/main/java/com/silva/bookstore/model/Author.java
@@ -1,26 +1,25 @@
 package com.silva.bookstore.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Getter
 @Entity
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table
 public class Author {
     @Id
     @GeneratedValue(generator = "sequence_generator", strategy = GenerationType.SEQUENCE)
     @SequenceGenerator(name = "sequence_generator", sequenceName = "sequence_generator", allocationSize = 1)
     private Long id;
-    @Getter
     private String firstName;
-    @Getter
     private String lastName;
-    @Getter
     private String email;
-    @Getter
     private String contactNo;
 
     public Author(String firstName, String lastName, String email, String contactNo) {
